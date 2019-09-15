f0af73b6d344d8f7142cc24edbcb2e9b8575b98a
david <david@david-VirtualBox>
2019-09-14 19:11:38 -0600

    Original code

---
+++ src/Hello World.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : David Sullivan
+// Version     :
+// Copyright   : Not at-all copyrighted
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
